237efe7eff471483195a87c5ee231761af913ae9
Maxwell Hankner <maxwellhankner@gmail.com>
2025-02-22 16:07:28 -0600

    removed unused packages

--- client/src/features/app/App.jsx
+++ client/src/features/app/App.jsx
@@ -8,7 +8,6 @@ import Login from '../auth/Login';
 import Profile from '../profile/Profile';
 import NoPage from '../error/NoPage';
 import { AppContainer } from './styledComponents';
-import 'sanitize.css';
 
 const theme = {
   colors: {
@@ -23,11 +22,81 @@ const shouldForwardProp = prop =>
   !['hsl', 'hsv', 'pointer'].includes(prop);
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  /* Box sizing rules */
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  /* Remove default margin and padding */
+  html,
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  p,
+  figure,
+  blockquote,
+  dl,
+  dd {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+  }
+
+  /* Set core root defaults */
+  html:focus-within {
+    scroll-behavior: smooth;
+  }
+
+  /* Set core body defaults */
+  body {
+    min-height: 100vh;
+    text-rendering: optimizeSpeed;
+    line-height: 1.5;
     font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  /* Remove list styles */
+  ul,
+  ol {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  /* Make images easier to work with */
+  img,
+  picture {
+    max-width: 100%;
+    display: block;
+  }
+
+  /* Inherit fonts for inputs and buttons */
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  /* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+  @media (prefers-reduced-motion: reduce) {
+    html:focus-within {
+      scroll-behavior: auto;
+    }
+    
+    *,
+    *::before,
+    *::after {
+      animation-duration: 0.01ms !important;
+      animation-iteration-count: 1 !important;
+      transition-duration: 0.01ms !important;
+      scroll-behavior: auto !important;
+    }
   }
 `;
 
