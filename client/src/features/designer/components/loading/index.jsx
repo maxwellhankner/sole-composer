20e4a7875057308f4b40ab015fb1fd934be861fb
Maxwell Hankner <maxwellhankner@gmail.com>
2025-02-26 01:03:31 -0600

    removed cn from errorscreen

--- client/src/features/designer/components/loading/index.jsx
+++ client/src/features/designer/components/loading/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { cn } from "../../../../lib/utils";
 
 export function LoadingSpinner({ message = "Loading..." }) {
     return (
@@ -12,30 +11,10 @@ export function LoadingSpinner({ message = "Loading..." }) {
     );
 } 
 
-export function LoadingScreen({ message = "Loading...", className, ...props }) {
+export function ErrorScreen({ error, ...props }) {
   return (
     <div 
-      className={cn(
-        "fixed inset-0 bg-white z-[1]",
-        className
-      )}
-      {...props}
-    >
-      <div className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 flex flex-col items-center">
-        <div className="w-[150px] h-[150px] border-[16px] border-[#e9e9e9] border-t-[#333333] rounded-full animate-spin"></div>
-        <p className="mt-4 text-[#333333] text-lg">{message}</p>
-      </div>
-    </div>
-  );
-}
-
-export function ErrorScreen({ error, className, ...props }) {
-  return (
-    <div 
-      className={cn(
-        "min-h-screen flex items-center justify-center bg-white",
-        className
-      )}
+      className="min-h-screen flex items-center justify-center bg-white"
       {...props}
     >
       <p className="text-red-500">Error: {error}</p>
